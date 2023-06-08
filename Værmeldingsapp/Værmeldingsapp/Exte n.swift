bdff7bd869806fd641f514567a8d67f47e6dace4
MagnusHN <magnushn05@gmail.com>
2023-06-09 00:03:11 +0200

    extention for værmelding

---
+++ Værmeldingsapp/Værmeldingsapp/Exte n.swift
@@ -0,0 +1,8 @@
+//
+//  Extentions.swift
+//  Værmeldingsapp
+//
+//  Created by Magnus Holth Nysveen on 08/06/2023.
+//
+
+import Foundation
---
+++ Værmeldingsapp/Værmeldingsapp/Extentions.swift
@@ -0,0 +1,14 @@
+//
+//  Extentions.swift
+//  Værmeldingsapp
+//
+//  Created by Magnus Holth Nysveen on 08/06/2023.
+//
+
+import Foundation
+
+extension Double{
+    func roundDouble() -> String{
+        return String(format: "%.0f", self)
+    }
+}
