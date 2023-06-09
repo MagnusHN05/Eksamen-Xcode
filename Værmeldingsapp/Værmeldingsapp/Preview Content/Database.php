50308be2f932b60fabe3ddf741f286779cd70be7
MagnusHN <magnushn05@gmail.com>
2023-06-09 13:14:26 +0200

    MySQLworkbench database med PHP

---
+++ Værmeldingsapp/Værmeldingsapp/Preview Content/Database.php
@@ -0,0 +1,38 @@
+
+$tjener = "localhost";
+
+   $brukernavn = "root";
+
+   $passord = "root";
+
+   $database = "xcodeeksamen";
+
+
+   $kobling = new mysqli($tjener, $brukernavn, $passord, $database);
+
+
+
+
+   if($kobling->connect_error) {
+
+       die("Noe gikk galt: " . $kobling->connect_error);
+
+   } else {
+
+       echo "Koblingen virker.<br>";
+
+   }
+
+$brukernavn = $_GET["brukernavn"];
+$Passord = $_GET["Passord"];
+
+$sql = "INSERT INTO Elev (Email, brukernavn, Passord) VALUES (,'$brukernavn','Passord')";
+
+if($kobling->query($sql)) {
+    echo "Spørringen $sql ble gjennomført.";
+} else {
+    echo "Noe gikk galt med spørringen $sql ($kobling->error).";
+}
+
+
+   $kobling->set_charset("utf8");
